173022f6c3c1a75bfd53b9ea8c5a8dc5a139f7c7
Michał Sarnacki <dzon444@outlook.com>
2023-04-16 10:06:28 +0200

    First file upload

---
+++ InvoiceManager/Models/Domains/Invoice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceManager.Models.Domains
+{
+    public class Invoice
+    {
+        public Invoice()
+        {
+            InvoicePositions = new Collection<InvoicePosition>();
+        }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Pole Tytuł jest wymagane!")]
+        [Display(Name = "Tytuł")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Pole Wartość jest wymagane!")]
+        [Display(Name = "Wartość")]
+        public decimal Value { get; set; }
+
+        [Required(ErrorMessage = "Pole Metoda płatności jest wymagane!")]
+        [Display(Name = "Metoda płatności")]
+        public int MethodOfPaymentId { get; set; }
+
+        [Required(ErrorMessage = "Pole Termin płatności jest wymagane!")]
+        [Display(Name = "Termin płatności")]
+        public DateTime PaymentDate { get; set; }
+
+        [Display(Name = "Data wystawienia")]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Uwagi")]
+        public string Comments { get; set; }
+
+        [Required(ErrorMessage = "Pole Klient jest wymagane!")]
+        [Display(Name = "Klient")]
+        public int ClientId { get; set; }
+       
+        [Required]
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public Client Client { get; set; }
+        public MethodOfPayment MethodOfPayment { get; set; }
+        public ICollection<InvoicePosition> InvoicePositions { get; set; }
+    }
+}
---
+++ InvoiceManager/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using InvoiceManager.Models.Domains;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceManager.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
